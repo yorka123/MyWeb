75bff75e8cc472916bd2bac7511fbe1afffdde34
York Zeng <york8662067@gmail.com>
2026-07-02 22:04:59 +0800

    I want die cuz the gallery update

--- popup.js
+++ popup.js
@@ -1,10 +1,21 @@
 
-const image = document.querySelector('.clickable_img');
+const images = document.querySelectorAll('.clickable_img');
 const popup = document.getElementById('popup');
+const selected_image = document.getElementById('selected_image');
 
-image.addEventListener("click", (event)=>{
-    const imgsrc = event.target.src;
+const image_background = document.getElementById('image_background');
 
-    console.log(imgsrc);
-    alert("Image URL: " + imgsrc);
+images.forEach((images) => {
+    images.addEventListener("click", (event) => {
+        const imgsrc = event.currentTarget.src;
+        const imgalt = event.currentTarget.alt;
+        selected_image.src = imgsrc;
+        popup.style.display = "contents";
+        image_background.style.visibility = "visible";
+    });
+});
+
+popup.addEventListener("click", (event)=>{
+    popup.style.display = "none";
+    image_background.style.visibility = "hidden";
 });
